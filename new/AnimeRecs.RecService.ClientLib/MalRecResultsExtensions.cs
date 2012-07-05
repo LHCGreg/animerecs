3a3944f9460636f35dd3899d852aa48d231b0c46
Greg Najda <gregnajda@gmail.com>
2012-07-04 21:15:26 -0400

    Missed some files in previous commits.

---
+++ new/AnimeRecs.RecService.ClientLib/MalRecResultsExtensions.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AnimeRecs.RecEngine;
+using AnimeRecs.RecService.DTO;
+
+namespace AnimeRecs.RecService.ClientLib
+{
+    public static class MalRecResultsExtensions
+    {
+        public static MalRecResults<IEnumerable<RecEngine.AverageScoreRecommendation>> AsAverageScoreResults(this MalRecResults<IEnumerable<IRecommendation>> basicResults)
+        {
+            if (basicResults.RecommendationType.Equals(RecommendationTypes.AverageScore, StringComparison.OrdinalIgnoreCase))
+            {
+                IEnumerable<RecEngine.AverageScoreRecommendation> results = (IEnumerable<RecEngine.AverageScoreRecommendation>)basicResults.Results;
+                return new MalRecResults<IEnumerable<RecEngine.AverageScoreRecommendation>>(results, basicResults.AnimeInfo, basicResults.RecommendationType);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static MalRecResults<RecEngine.MAL.MalAnimeRecsResults> AsAnimeRecsResults(this MalRecResults<IEnumerable<IRecommendation>> basicResults)
+        {
+            if (basicResults.RecommendationType.Equals(RecommendationTypes.AnimeRecs, StringComparison.OrdinalIgnoreCase))
+            {
+                RecEngine.MAL.MalAnimeRecsResults results = (RecEngine.MAL.MalAnimeRecsResults)basicResults.Results;
+                return new MalRecResults<RecEngine.MAL.MalAnimeRecsResults>(results, basicResults.AnimeInfo, basicResults.RecommendationType);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static MalRecResults<IEnumerable<AnimeRecs.RecEngine.MostPopularRecommendation>> AsMostPopularResults(this MalRecResults<IEnumerable<IRecommendation>> basicResults)
+        {
+            if (basicResults.RecommendationType.Equals(RecommendationTypes.MostPopular, StringComparison.OrdinalIgnoreCase))
+            {
+                IEnumerable<AnimeRecs.RecEngine.MostPopularRecommendation> results = (IEnumerable<AnimeRecs.RecEngine.MostPopularRecommendation>)basicResults.Results;
+                return new MalRecResults<IEnumerable<RecEngine.MostPopularRecommendation>>(results, basicResults.AnimeInfo, basicResults.RecommendationType);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static MalRecResults<IEnumerable<AnimeRecs.RecEngine.RatingPredictionRecommendation>> AsRatingPredictionResults(this MalRecResults<IEnumerable<IRecommendation>> basicResults)
+        {
+            if (basicResults.RecommendationType.Equals(RecommendationTypes.RatingPrediction, StringComparison.OrdinalIgnoreCase))
+            {
+                IEnumerable<AnimeRecs.RecEngine.RatingPredictionRecommendation> results = (IEnumerable<AnimeRecs.RecEngine.RatingPredictionRecommendation>)basicResults.Results;
+                return new MalRecResults<IEnumerable<RecEngine.RatingPredictionRecommendation>>(results, basicResults.AnimeInfo, basicResults.RecommendationType);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
+
+// Copyright (C) 2012 Greg Najda
+//
+// This file is part of AnimeRecs.RecService.ClientLib.
+//
+// AnimeRecs.RecService.ClientLib is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// AnimeRecs.RecService.ClientLib is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with AnimeRecs.RecService.ClientLib.  If not, see <http://www.gnu.org/licenses/>.
---
+++ new/AnimeRecs.RecService/OperationHandlers/GetRecSourceTypeHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AnimeRecs.RecService.DTO;
+
+namespace AnimeRecs.RecService.OperationHandlers
+{
+    internal static partial class OpHandlers
+    {
+        public static Response GetRecSourceType(Operation baseOperation, RecServiceState state, OperationCaster opCaster)
+        {
+            Operation<GetRecSourceTypeRequest> operation = (Operation<GetRecSourceTypeRequest>)baseOperation;
+            string recSourceType = state.GetRecSourceType(operation.Payload.RecSourceName);
+            return new Response<GetRecSourceTypeResponse>()
+            {
+                Body = new GetRecSourceTypeResponse(recSourceType)
+            };
+        }
+    }
+}
+
+// Copyright (C) 2012 Greg Najda
+//
+// This file is part of AnimeRecs.RecService.
+//
+// AnimeRecs.RecService is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// AnimeRecs.RecService is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with AnimeRecs.RecService.  If not, see <http://www.gnu.org/licenses/>.
