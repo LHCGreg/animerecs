0e9a3c844e079e260c88a007a3243d47802595f6
Greg Najda <gregnajda@gmail.com>
2012-07-11 22:01:11 -0400

    License footers for files added in last commit.

--- AnimeRecs.Web/Logging.cs
+++ AnimeRecs.Web/Logging.cs
@@ -10,3 +10,20 @@ namespace AnimeRecs.Web
         internal static Common.Logging.ILog Log { get { return Common.Logging.LogManager.GetLogger("AnimeRecs.Web"); } }
     }
 }
+
+// Copyright (C) 2012 Greg Najda
+//
+// This file is part of AnimeRecs.Web.
+//
+// AnimeRecs.Web is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// AnimeRecs.Web is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with AnimeRecs.Web.  If not, see <http://www.gnu.org/licenses/>.
