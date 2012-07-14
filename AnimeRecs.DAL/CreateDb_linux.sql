cf13bd4b906e5682e8132121fb79ad8f6ef55c1a
Greg Najda <gregnajda@gmail.com>
2012-07-13 23:58:07 -0400

    Split CreateDb.sql into Windows and Linux versions. The encoding names are slightly different.

---
+++ AnimeRecs.DAL/CreateDb_linux.sql
@@ -0,0 +1,6 @@
+﻿-- For PostgreSQL only
+
+CREATE DATABASE animerecs
+       ENCODING = 'UTF8'
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8'
---
+++ AnimeRecs.RecService/Configuration/ConfigRoot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AnimeRecs.RecService.DTO;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace AnimeRecs.RecService.Configuration
+{
+    class ConfigRoot
+    {
+        public IList<LoadRecSourceRequest> RecSources { get; set; }
+
+        public ConfigRoot()
+        {
+            RecSources = new List<LoadRecSourceRequest>();
+        }
+
+        public ConfigRoot(IList<LoadRecSourceRequest> recSources)
+        {
+            RecSources = recSources;
+        }
+
+        public static ConfigRoot LoadFromFile(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            ConfigRoot config = JsonConvert.DeserializeObject<ConfigRoot>(json);
+            foreach (LoadRecSourceRequest recSource in config.RecSources)
+            {
+
+            }
+        }
+    }
+}
