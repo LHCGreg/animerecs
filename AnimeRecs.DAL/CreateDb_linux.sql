9b48e64fd8c8bcfab4aa881e7dfb306eec54e624
Greg Najda <gregnajda@gmail.com>
2012-09-07 23:17:45 -0400

    Added semicolons to the CreateDb scripts.

--- AnimeRecs.DAL/CreateDb_linux.sql
+++ AnimeRecs.DAL/CreateDb_linux.sql
@@ -3,4 +3,4 @@
 CREATE DATABASE animerecs
        ENCODING = 'UTF8'
        LC_COLLATE = 'en_US.UTF-8'
-       LC_CTYPE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8';
--- AnimeRecs.DAL/CreateDb_windows.sql
+++ AnimeRecs.DAL/CreateDb_windows.sql
@@ -3,4 +3,4 @@
 CREATE DATABASE animerecs
        ENCODING = 'UTF8'
        LC_COLLATE = 'English_United States.1252'
-       LC_CTYPE = 'English_United States.1252'
+       LC_CTYPE = 'English_United States.1252';
