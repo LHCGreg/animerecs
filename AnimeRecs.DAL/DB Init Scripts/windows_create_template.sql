aec9330354a0116e70d549e84be0685c0b834233
Greg Najda <gregnajda@gmail.com>
2014-09-29 14:07:51 -0400

    dbsc-ify DB scripts.

--- AnimeRecs.DAL/DB Init Scripts/windows_create_template.sql
+++ AnimeRecs.DAL/DB Init Scripts/windows_create_template.sql
@@ -1,6 +1,6 @@
 ﻿-- For PostgreSQL only
 
-CREATE DATABASE animerecs
+CREATE DATABASE $DatabaseName$
        ENCODING = 'UTF8'
        LC_COLLATE = 'English_United States.1252'
        LC_CTYPE = 'English_United States.1252';
