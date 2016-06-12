a6df2c32d04b742a30cc3b8a5ef6fc2300817a73
Greg Najda <gregnajda@gmail.com>
2016-06-12 14:26:02 -0400

    Work around Funimation CMS weirdness for PUCHIM@S in UpdateStreams.

--- AnimeRecs.UpdateStreams/HtmlRegexAnimeStreamInfoSource.cs
+++ AnimeRecs.UpdateStreams/HtmlRegexAnimeStreamInfoSource.cs
@@ -96,6 +96,13 @@ namespace AnimeRecs.UpdateStreams
                     absoluteUrl = new Uri(animeListUri, rawUri).ToString();
                 }
 
+                // Hack to work around Funimation's CMS having this where "PUCHIM@S" would normally be:
+                // <span class=""__cf_email__"" data-cfemail=""29797c6a616064697a"">[email protected]</span><script data-cfhash='f9e31' type=""text/javascript"">/* <![CDATA[ */!function(t,e,r,n,c,a,p){try{t=document.currentScript||function(){for(t=document.getElementsByTagName('script'),e=t.length;e--;)if(t[e].getAttribute('data-cfhash'))return t[e]}();if(t&&(c=t.previousSibling)){p=t.parentNode;if(a=c.getAttribute('data-cfemail')){for(e='',r='0x'+a.substr(0,2)|0,n=2;a.length-n;n+=2)e+='%'+('0'+('0x'+a.substr(n,2)^r).toString(16)).slice(-2);p.replaceChild(document.createTextNode(decodeURIComponent(e)),c)}p.removeChild(t)}}catch(u){}}()/* ]]> */</script>
+                if (absoluteUrl == "http://www.funimation.com/shows/puchims/videos/episodes")
+                {
+                    animeName = "PUCHIM@S";
+                }
+
                 streams.Add(new AnimeStreamInfo(animeName: animeName, url: absoluteUrl, service: Service));
 
                 match = match.NextMatch();
