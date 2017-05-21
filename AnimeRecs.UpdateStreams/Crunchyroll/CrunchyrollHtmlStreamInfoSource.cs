07b3bb2896163e8b765b245e10b622274960cfe6
Greg Najda <gregnajda@gmail.com>
2017-05-21 19:53:49 -0400

    [UpdateStreams] Hack to work around Crunchyroll email address obfuscation.
    Crunchyroll messes with "THE IDOLM@STER CINDERELLA GIRLS Theater", replacing
    "IDOLM@STER" with some html that get replaced by javascript.

--- AnimeRecs.UpdateStreams/Crunchyroll/CrunchyrollHtmlStreamInfoSource.cs
+++ AnimeRecs.UpdateStreams/Crunchyroll/CrunchyrollHtmlStreamInfoSource.cs
@@ -34,7 +34,20 @@ namespace AnimeRecs.UpdateStreams.Crunchyroll
             HtmlRegexAnimeStreamInfoSource regexSource = new HtmlRegexAnimeStreamInfoSource(AnimeListUrl,
                 AnimeRegex.Value, _html, StreamingService.Crunchyroll,
                 animeNameContext: HtmlRegexContext.Body, urlContext: HtmlRegexContext.Attribute);
-            return regexSource.GetAnimeStreamInfo();
+
+            ICollection<AnimeStreamInfo> streams = regexSource.GetAnimeStreamInfo();
+            List<AnimeStreamInfo> fixedStreams = new List<AnimeStreamInfo>(streams);
+            for (int i = 0; i < fixedStreams.Count; i++)
+            {
+                // Hack to work around Crunchyroll having this where "IDOLM@STER" would normally be:
+                // <span class=""__cf_email__"" data-cfemail=""8fc6cbc0c3c2cfdcdbcadd"">[email protected]</span><script data-cfhash='f9e31' type=""text/javascript"">/* <![CDATA[ */!function(t,e,r,n,c,a,p){try{t=document.currentScript||function(){for(t=document.getElementsByTagName('script'),e=t.length;e--;)if(t[e].getAttribute('data-cfhash'))return t[e]}();if(t&&(c=t.previousSibling)){p=t.parentNode;if(a=c.getAttribute('data-cfemail')){for(e='',r='0x'+a.substr(0,2)|0,n=2;a.length-n;n+=2)e+='%'+('0'+('0x'+a.substr(n,2)^r).toString(16)).slice(-2);p.replaceChild(document.createTextNode(decodeURIComponent(e)),c)}p.removeChild(t)}}catch(u){}}()/* ]]> */</script>
+                if (fixedStreams[i].Url == "http://www.crunchyroll.com/the-idolmster-cinderella-girls-theater")
+                {
+                    fixedStreams[i] = new AnimeStreamInfo("THE IDOLM@STER CINDERELLA GIRLS Theater", "http://www.crunchyroll.com/the-idolmster-cinderella-girls-theater", StreamingService.Crunchyroll);
+                }
+            }
+
+            return fixedStreams;
         }
     }
 }
