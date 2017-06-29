45d47edeb84bd1ae1edf5c13ff51902bcf94ced4
Greg Najda <gregnajda@gmail.com>
2017-06-28 22:20:50 -0400

    Rename AnimeRecs.RecEngine.MAL.Tests to UnitTests to better describe.

--- AnimeRecs.RecEngine.MAL.UnitTests/ReadOnlyMalListEntryDictionaryTests.cs
+++ AnimeRecs.RecEngine.MAL.UnitTests/ReadOnlyMalListEntryDictionaryTests.cs
@@ -6,7 +6,7 @@ using NUnit.Framework;
 using AnimeRecs.RecEngine.MAL;
 using MalApi;
 
-namespace AnimeRecs.RecEngine.MAL.Tests
+namespace AnimeRecs.RecEngine.MAL.UnitTests
 {
     [TestFixture]
     public class ReadOnlyMalListEntryDictionaryTests
@@ -35,19 +35,19 @@ namespace AnimeRecs.RecEngine.MAL.Tests
     }
 }
 
-// Copyright (C) 2012 Greg Najda
+// Copyright (C) 2017 Greg Najda
 //
-// This file is part of AnimeRecs.RecEngine.MAL.Tests.
+// This file is part of AnimeRecs.RecEngine.MAL.UnitTests.
 //
-// AnimeRecs.RecEngine.MAL.Tests is free software: you can redistribute it and/or modify
+// AnimeRecs.RecEngine.MAL.UnitTests is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// AnimeRecs.RecEngine.MAL.Tests is distributed in the hope that it will be useful,
+// AnimeRecs.RecEngine.MAL.UnitTests is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 //  You should have received a copy of the GNU General Public License
-//  along with AnimeRecs.RecEngine.MAL.Tests.  If not, see <http://www.gnu.org/licenses/>.
+//  along with AnimeRecs.RecEngine.MAL.UnitTests.  If not, see <http://www.gnu.org/licenses/>.
--- AnimeRecs.RecEngine.MAL/Properties/AssemblyInfo.cs
+++ AnimeRecs.RecEngine.MAL/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: InternalsVisibleTo("AnimeRecs.RecEngine.MAL.Tests")]
+[assembly: InternalsVisibleTo("AnimeRecs.RecEngine.MAL.UnitTests")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
